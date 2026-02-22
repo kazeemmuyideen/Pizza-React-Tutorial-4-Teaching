3f1648cacc19b10e64ea7192bfaed0954919dc5d
Kazeem Muyideen <KkazeemMuyideen@gmail.com>
2026-02-22 22:44:45 +0100

    Conditional rendering using ternary operator

--- src/index.js
+++ src/index.js
@@ -26,8 +26,8 @@ function Header() {
 }
 
 function Menu() {
-  const pizzas = pizzaData;
-  //const pizzas = []; //it returns the tag becos empty array is truthy value
+  // const pizzas = pizzaData;
+  const pizzas = []; //it returns the tag becos empty array is truthy value
   const numPizzas = pizzas.length;
 
   return (
@@ -35,12 +35,14 @@ function Menu() {
       <h2>Our Menu</h2>
 
       {
-        /*pizzas*/ numPizzas > 0 && (
+        /*pizzas*/ numPizzas > 0 ? (
           <ul className="pizzas">
             {pizzas.map((pizza) => (
               <Pizza pizzaObj={pizza} key={pizza.name} />
             ))}
           </ul>
+        ) : (
+          <p>We're still working on our menu. Please come back later :)</p>
         )
       }
 
@@ -76,17 +78,21 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 20;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online!</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
