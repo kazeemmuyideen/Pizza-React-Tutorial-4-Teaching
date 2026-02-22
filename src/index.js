2803f878b6f149900a8ebd087914b1e10cef6b30
Kazeem Muyideen <KkazeemMuyideen@gmail.com>
2026-02-22 15:44:35 +0100

    done with minor settings

--- src/index.js
+++ src/index.js
@@ -30,9 +30,9 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul>
+      <ul className="pizzas">
         {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} />
+          <Pizza pizzaObj={pizza} key={pizza.name} />
         ))}
       </ul>
 
@@ -55,14 +55,14 @@ function Menu() {
 
 function Pizza(props) {
   return (
-    <div className="pizza">
+    <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}></img>
-      <li>
+      <div>
         <h3>{props.pizzaObj.name}</h3>
         <p>{props.pizzaObj.ingredients}</p>
         <span>{props.pizzaObj.price}</span>
-      </li>
-    </div>
+      </div>
+    </li>
   );
 }
 
